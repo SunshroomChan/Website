c6816a8af19b2bae0824b39bd4606f6be307df6a
HoangNotGay <89954408+SunshroomChan@users.noreply.github.com>
2024-10-06 11:40:41 +0700

    Update register.php

--- register.php
+++ register.php
@@ -6,7 +6,7 @@ $password = "Hoangviphb1234.";
 $dbname = "testdb";
 
 // Tạo kết nối
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Kiểm tra kết nối
 if ($conn->connect_error) {
@@ -16,36 +16,16 @@ if ($conn->connect_error) {
 // Chọn cơ sở dữ liệu
 $conn->select_db($dbname);
 
-// Tạo bảng phanquyen nếu chưa tồn tại
-$sql1 = "CREATE TABLE IF NOT EXISTS phanquyen (
-    id_role INT(11) AUTO_INCREMENT PRIMARY KEY,
-    role_name VARCHAR(255) NOT NULL
-)";
-if ($conn->query($sql1) !== TRUE) {
-    die("Lỗi khi tạo bảng phanquyen: " . $conn->error);
-}
-
-// Thêm các vai trò nếu chưa tồn tại
-$roles = ["Admin", "NhanVien", "KhachHang"];
-foreach ($roles as $role) {
-    $sql_role = "INSERT INTO phanquyen (role_name) SELECT * FROM (SELECT '$role') AS tmp WHERE NOT EXISTS (SELECT role_name FROM phanquyen WHERE role_name = '$role') LIMIT 1";
-    if ($conn->query($sql_role) !== TRUE) {
-        die("Lỗi khi thêm vai trò: " . $conn->error);
-    }
-}
-
 // Tạo bảng khachhang nếu chưa tồn tại
 $sql2 = "CREATE TABLE IF NOT EXISTS khachhang (
     id_kh INT(11) AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    sdt VARCHAR(255) NULL,
+    sdt VARCHAR(20) NULL,
     password VARCHAR(255) NOT NULL,
     ngaysinh DATE NOT NULL,
     gioitinh VARCHAR(255) NOT NULL,
     anhavt LONGBLOB NOT NULL,
-    id_role INT NOT NULL,
-    FOREIGN KEY (id_role) REFERENCES phanquyen(id_role),
     CREATE_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UPDATE_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
@@ -74,14 +54,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Mã hóa mật khẩu
             $password_hashed = password_hash($pass, PASSWORD_DEFAULT);
 
-            // Phân quyền cho người dùng
-            // Note: 1 - Admin, 2 - NhanVien, 3 - KhachHang
-            $role = 3;
-
             // Chuẩn bị truy vấn SQL để thêm người dùng mới
-            $sql = "INSERT INTO khachhang (username, email, password, id_role) VALUES (?, ?, ?, ?)";
+            $sql = "INSERT INTO khachhang (username, email, password) VALUES (?, ?, ?)";
             $stmt = $conn->prepare($sql);
-            $stmt->bind_param("sssi", $user, $email, $password_hashed, $role);
+            $stmt->bind_param("sss", $user, $email, $password_hashed);
 
             // Thực thi truy vấn và kiểm tra kết quả
             if ($stmt->execute()) {
